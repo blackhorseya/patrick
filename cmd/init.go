523a3d3d04037a99fa64ec937d4bc21d6e04ccac
Sean Zheng <blackhorseya@gmail.com>
2022-09-22 02:29:20 +0800

    feat: added init command

---
+++ cmd/init.go
@@ -0,0 +1,120 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/blackhorseya/patrick/pkg/entity/project"
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:     "init [name]",
+	Aliases: []string{"create"},
+	Short:   "Initialize a Project",
+	Long:    `Initialize a Project`,
+	Run: func(cmd *cobra.Command, args []string) {
+		projectPath, err := initializeProject(args)
+		cobra.CheckErr(err)
+
+		// todo: 2022/9/22|sean|impl me
+
+		fmt.Printf("Your Cobra application is ready at\n%s\n", projectPath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initializeProject(args []string) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	if len(args) > 0 {
+		if args[0] != "." {
+			wd = fmt.Sprintf("%s/%s", wd, args[0])
+		}
+	}
+
+	modName := getModImportPath()
+
+	prj := &project.Info{
+		AbsolutePath: wd,
+		PkgName:      modName,
+		AppName:      path.Base(modName),
+	}
+
+	err = prj.Create()
+	if err != nil {
+		return "", err
+	}
+
+	return prj.AbsolutePath, nil
+}
+
+func getModImportPath() string {
+	mod, cd := parseModInfo()
+	return path.Join(mod.Path, fileToURL(strings.TrimPrefix(cd.Dir, mod.Dir)))
+}
+
+func fileToURL(in string) string {
+	i := strings.Split(in, string(filepath.Separator))
+	return path.Join(i...)
+}
+
+func parseModInfo() (Mod, CurDir) {
+	var mod Mod
+	var dir CurDir
+
+	m := modInfoJSON("-m")
+	cobra.CheckErr(json.Unmarshal(m, &mod))
+
+	// Unsure why, but if no module is present Path is set to this string.
+	if mod.Path == "command-line-arguments" {
+		cobra.CheckErr("Please run `go mod init <MODNAME>` before `patrick init`")
+	}
+
+	e := modInfoJSON("-e")
+	cobra.CheckErr(json.Unmarshal(e, &dir))
+
+	return mod, dir
+}
+
+type Mod struct {
+	Path, Dir, GoMod string
+}
+
+type CurDir struct {
+	Dir string
+}
+
+func goGet(mod string) error {
+	return exec.Command("go", "get", mod).Run()
+}
+
+func modInfoJSON(args ...string) []byte {
+	cmdArgs := append([]string{"list", "-json"}, args...)
+	out, err := exec.Command("go", cmdArgs...).Output()
+	cobra.CheckErr(err)
+
+	return out
+}
--- pkg/entity/project/project.go
+++ pkg/entity/project/project.go
@@ -1,8 +1,30 @@
 package project
 
+import (
+	"fmt"
+	"os"
+)
+
 // Info define a project information
 type Info struct {
 	PkgName      string `json:"pkg_name"`
 	AbsolutePath string `json:"absolute_path"`
 	AppName      string `json:"app_name"`
+}
+
+func (p *Info) Create() error {
+	// check if AbsolutePath exists
+	if _, err := os.Stat(p.AbsolutePath); os.IsNotExist(err) {
+		// create directory
+		err = os.Mkdir(p.AbsolutePath, 0754)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("Starting create project...")
+
+	// todo: 2022/9/22|sean|impl me
+
+	return nil
 }
