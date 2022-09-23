8df1ba9900e3aadb21cc3624e611fa549824ff7b
Sean Zheng <blackhorseya@gmail.com>
2022-09-24 05:27:21 +0800

    fix: test

--- internal/app/patrick/biz/project/impl_test.go
+++ internal/app/patrick/biz/project/impl_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/blackhorseya/patrick/internal/app/patrick/biz/project/repo"
-	"github.com/blackhorseya/patrick/internal/pkg/entity/project"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -29,31 +28,4 @@ func (s *SuiteTest) TearDownTest() {
 
 func TestSuiteTest(t *testing.T) {
 	suite.Run(t, new(SuiteTest))
-}
-
-func (s *SuiteTest) Test_impl_InitProject() {
-	type args struct {
-		prj  *project.Info
-		mock func()
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		s.T().Run(tt.name, func(t *testing.T) {
-			if tt.args.mock != nil {
-				tt.args.mock()
-			}
-
-			if err := s.biz.InitProject(tt.args.prj); (err != nil) != tt.wantErr {
-				t.Errorf("InitProject() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			s.repo.AssertExpectations(t)
-		})
-	}
 }
